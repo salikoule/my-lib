8821e59c6eaf019d370bd06b67e87ee6c64c3b74
koulesali@gmail.com <koulesali@gmail.com>
2023-04-15 07:04:32 +0000

    Edited settings and Form1

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -1,11 +1,13 @@
 from ._anvil_designer import Form1Template
 from anvil import *
 
+import anvil.js
+
 class Form1(Form1Template):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
-
+    uuid_module = require(anvil.js.import_from('https://cdnjs.cloudflare.com/ajax/libs/shortid/2.2.16/generate.min.js'))
     #self.gantt_chart_1.add_tasks()
     # self.gantt_chart_1.add_event_handler('x-on_click', self.on_click)
     
