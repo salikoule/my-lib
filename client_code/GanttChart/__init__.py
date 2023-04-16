bc18eaaa5b776e0e960e48e00967ec8ebe66b88a
koulesali@gmail.com <koulesali@gmail.com>
2023-04-16 17:39:35 +0000

    Edited GanttChart

--- client_code/GanttChart/__init__.py
+++ client_code/GanttChart/__init__.py
@@ -16,31 +16,63 @@ class GanttChart(GanttChartTemplate):
     self.column_panel_1.clear()
     gantt = anvil.js.get_dom_node(self.column_panel_1)
     tasks = [
-      {
-    	'id': 'Task 1',
-    	'name': 'Redesign website',
-    	'start': '2016-12-28',
-    	'end': '2016-12-31',
-    	'progress': 20,
-    	'dependencies': 'Task 2, Task 3'
-      },
-      {
-    	'id': 'Task 2',
-    	'name': 'New login Page',
-    	'start': '2016-12-28',
-    	'end': '2016-12-31',
-    	'progress': 50,
-    	'dependencies': None
-      },
-      {
-    	'id': 'Task 3',
-    	'name': 'New theme',
-    	'start': '2016-12-28',
-    	'end': '2016-12-31',
-    	'progress': 80,
-    	'dependencies': None
-      }
-    ]
+			{
+				'start': '2018-10-01',
+				'end': '2018-10-08',
+				'name': 'Redesign website',
+				'id': "Task 0",
+				'progress': 20
+			},
+			{
+				'start': '2018-10-03',
+				'end': '2018-10-06',
+				'name': 'Write new content',
+				'id': "Task 1",
+				'progress': 5,
+				'dependencies': 'Task 0'
+			},
+			{
+				'start': '2018-10-04',
+				'end': '2018-10-08',
+				'name': 'Apply new styles',
+				'id': "Task 2",
+				'progress': 10,
+				'dependencies': 'Task 1'
+			},
+			{
+				'start': '2018-10-08',
+				'end': '2018-10-09',
+				'name': 'Review',
+				'id': "Task 3",
+				'progress': 5,
+				'dependencies': 'Task 2'
+			},
+			{
+				'start': '2018-10-08',
+				'end': '2018-10-10',
+				'name': 'Deploy',
+				'id': "Task 4",
+				'progress': 0,
+				'dependencies': 'Task 2'
+			},
+			{
+				'start': '2018-10-11',
+				'end': '2018-10-11',
+				'name': 'Go Live!',
+				'id': "Task 5",
+				'progress': 0,
+				'dependencies': 'Task 4',
+				'custom_class': 'bar-milestone'
+			},
+			# {
+			# 	'start': '2014-01-05',
+			# 	'end': '2019-10-12',
+			# 	'name': 'Long term task',
+			# 	'id': "Task 6",
+			# 	'progress': 0
+			# }
+		]
+    #print(dir(Gantt))
     self.gantt = Gantt(gantt, tasks,
     #  {
     # 'header_height': 50,
@@ -56,15 +88,25 @@ class GanttChart(GanttChartTemplate):
     # #'language': 'en', // or 'es', 'it', 'ru', 'ptBr', 'fr', 'tr', 'zh', 'de', 'hu',
     # 'custom_popup_html': None
     # },
-    {'on_click': self.on_click}
+    {'on_click': self.on_click, 'on_date_change': self.on_date_change,'on_progress_change': self.on_progress_change,'on_view_change': self.on_view_change,}
                       )
     #self.gantt.on('on_click')
+    #print(dict(self.gantt))
 
   def link_click(self, **event_args):
     self.gantt.change_view_mode(event_args['sender'].tag)
 
   def on_click(self, task):
     print(task)
+
+  def on_date_change(self, task, start, end):
+    print(task, start, end)
+
+  def on_progress_change(self, task, progress):
+    print(task, progress)
+
+  def on_view_change(self, mode):
+    print(mode)
     
 
 
