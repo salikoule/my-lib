ffb301732c59c56d1c78417ae2be1a9e3199a050
koulesali@gmail.com <koulesali@gmail.com>
2023-04-18 10:57:47 +0000

    Created FullCalendar_; Edited settings and Form1

---
+++ client_code/FullCalendar_/__init__.py
@@ -0,0 +1,57 @@
+from ._anvil_designer import FullCalendar_Template
+from anvil import *
+from anvil.js.window import FullCalendar
+import anvil.js
+
+class FullCalendar_(FullCalendar_Template):
+  def __init__(self, **properties):
+    # Set Form properties and Data Bindings.
+    self.init_components(**properties)
+
+    # Any code you write here will run before the form opens.
+
+  def form_show(self, **event_args):
+    self.column_panel.clear()
+    calendar = anvil.js.get_dom_node(self.column_panel)
+    self.calendar = FullCalendar.Calendar(
+      calendar, 
+      {
+        'initialView': 'dayGridMonth',
+        'selectable': True,
+        'firstDay':1,
+        'dateClick':self.date_click,
+        'eventClick':self.event_click,
+        'events': [
+            { 
+              'title': 'The Title', 
+              'start': '2023-04-19', 
+              'end': '2023-04-19'
+            },
+          { 
+              'title': 'The Event', 
+              'allDay': True, 
+              'start': '2023-04-20', 
+              'end': '2023-04-20'
+            },
+        ]
+      },
+    )
+
+    self.calendar.render()
+    
+
+  def date_click(self, info):
+    #alert(info['dateStr'])
+    print(info)
+    info.dayEl.style.backgroundColor = 'red'
+
+  def event_click(self, info):
+    print(info.event.title)
+    info.el.style.backgroundColor = 'red'
+
+  def button_1_click(self, **event_args):
+    self.calendar.addEvent({ 
+              'title': 'Ad Hoc', 
+              'start': '2023-04-22', 
+              'end': '2023-04-22'
+            })
