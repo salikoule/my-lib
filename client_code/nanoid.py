e6906c6536785b7fe5ed4ebc0f202735a3b1d126
koulesali@gmail.com <koulesali@gmail.com>
2023-04-19 17:03:14 +0000

    Edited settings and nanoid

--- client_code/nanoid.py
+++ client_code/nanoid.py
@@ -1,16 +1,7 @@
-# This is a module.
-# You can define variables and functions here, and use them from any form. For example, in a top-level form:
-#
-#    from . import Module1
-#
-#    Module1.say_hello()
-#
-
 import anvil.js
-#uuid_module = anvil.js.import_from('https://cdn.jsdelivr.net/npm/nanoid/nanoid.js')
-uuid_module = anvil.js.import_from('https://cdnjs.cloudflare.com/ajax/libs/nanoid/4.0.2/nanoid.min.js')
+#nanoid_module = anvil.js.import_from('https://cdn.jsdelivr.net/npm/nanoid/nanoid.js')
+nanoid_module = anvil.js.import_from('https://cdnjs.cloudflare.com/ajax/libs/nanoid/4.0.2/nanoid.min.js')
 
-print(dir(uuid_module))
-for i in range(100):
-  print(uuid_module.nanoid(11))
-#print(uuid_module.customAlphabet('1234567890abcdef', 10))
+def nanoid(num=11):
+  """Returns a nanoid of default 11 characters. Argument num is to specify the number of characters to return"""
+  return nanoid_module.nanoid(num)
