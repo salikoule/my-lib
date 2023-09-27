6586bf6dbdcb97d832b3107fba5c88b0077c66d9
koulesali@gmail.com <koulesali@gmail.com>
2023-09-27 01:29:56 +0000

    Created Treant.css; Edited ServerModule1, TreantJS, settings, vendor/jquery.easing.js and 2 more; Deleted TreantLib

--- server_code/ServerModule1.py
+++ server_code/ServerModule1.py
@@ -1,6 +1,5 @@
 import anvil.server
-from anytree import Node, AnyNode, RenderTree
-from anytree.exporter import JsonExporter
+
 
 # This is a server module. It runs on the Anvil server,
 # rather than in the user's browser.
@@ -9,12 +8,5 @@ from anytree.exporter import JsonExporter
 # them with @anvil.server.callable.
 # Here is an example - you can replace it with your own:
 #
-@anvil.server.callable
-def get_tree():
-  parent = AnyNode(text = {'name':'Parent'})
-  node = AnyNode(text = {'name':'Child 1'}, parent = parent)
-  node = AnyNode(text = {'name':'Child 2'}, parent = parent)
-  exporter = JsonExporter(sort_keys=True)
-  print(RenderTree(parent))
-  return exporter.export(parent)
+
 
