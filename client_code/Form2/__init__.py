ed7f7930291a58d4e072c8287f8d0e7f4d0dd2ea
koulesali@gmail.com <koulesali@gmail.com>
2023-10-10 04:34:05 +0000

    Edited settings and Form2

--- client_code/Form2/__init__.py
+++ client_code/Form2/__init__.py
@@ -1,13 +1,22 @@
 from ._anvil_designer import Form2Template
 from anvil import *
 import anvil.server
-from anvil.js.window import timepicker
+from 
+
+_js_timepicker = anvil.js.import_from("//cdnjs.cloudflare.com/ajax/libs/timepicker/1.3.5/jquery.timepicker.min.js")
 
 class Form2(Form2Template):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
-
-    print(dir(w))
+    
+    print(dir(_js_timepicker))
 
     # Any code you write here will run before the form opens.
+
+  def form_show(self, **event_args):
+    self.column_panel.clear()
+    panel = anvil.js.get_dom_node(self.column_panel)
+    self.timepicker = _js_timepicker.timepicker(panel)
+    
+
