c54349b562f8bc2be7faf1af926bb993678e8bb2
koulesali@gmail.com <koulesali@gmail.com>
2023-11-14 20:35:21 +0000

    Edited settings and Components.ChartJS

--- client_code/Components/ChartJS/__init__.py
+++ client_code/Components/ChartJS/__init__.py
@@ -1,6 +1,7 @@
 from ._anvil_designer import ChartJSTemplate
 from anvil import *
 import anvil.server
+from anvil.js.window import Chart
 
 class ChartJS(ChartJSTemplate):
   def __init__(self, **properties):
@@ -8,3 +9,42 @@ class ChartJS(ChartJSTemplate):
     self.init_components(**properties)
 
     # Any code you write here will run before the form opens.
+
+  @property
+  def labels(self):
+    return self._labels
+  
+  @labels.setter
+  def labels(self, l):
+    self._labels = l
+
+  @property
+  def data(self):
+    return self._data
+  
+  @data.setter
+  def data(self, d):
+    self._data = d
+
+  @property
+  def type(self):
+    return self._type
+  
+  @type.setter
+  def type(self, t):
+    self._type = t
+
+  @property
+  def height(self):
+    return self._height
+  
+  @height.setter
+  def height(self, h):
+    self._height = h
+
+  def form_show(self, **event_args):
+    self.clear()
+    canvas = Canvas(height=self.height)
+    self.add_component(canvas, full_width_row=True)
+    ctx = js.get_dom_node(canvas)
+    Chart(ctx, config)
