9073b8204b883ce05d8aa7e57f13a6690aed90e7
koulesali@gmail.com <koulesali@gmail.com>
2024-03-06 19:58:06 +0000

    Edited settings, ServUtils and custom package dependencies; Deleted Form2

--- server_code/ServUtils.py
+++ server_code/ServUtils.py
@@ -1,15 +1,1 @@
-import anvil.server
-import gradio as gr
-
-def greet(name, intensity):
-    return "Hello, " + name + "!" * int(intensity)
-
-@anvil.server.callable
-def get_interface():
-  demo = gr.Interface(
-    fn=greet,
-    inputs=["text", "slider"],
-    outputs=["text"],
-  )
-  return demo
 
