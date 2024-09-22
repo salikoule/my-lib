16c1303a55f551ee23ba55f09928801a8afdc24b
koulesali@gmail.com <koulesali@gmail.com>
2024-09-22 09:38:31 +0000

    Edited DatetimeHandler and Utils

--- client_code/Utils/__init__.py
+++ client_code/Utils/__init__.py
@@ -10,3 +10,15 @@ import anvil.server
 def centered_alert(content, title = "", buttons=None, large=False):
   """Centered Aligned Alert with blurred background effect"""
   return anvil.alert(content, title=title, buttons=buttons, large=large, role='center-alert')
+
+def remove_trailing_spaces(text: str) -> str:
+    """
+    Removes trailing spaces from the given text.
+
+    Args:
+        text (str): The text to remove trailing spaces from.
+
+    Returns:
+        str or None: The text with trailing spaces removed, or None if the input is None.
+    """
+    return text.rstrip() if text is not None else None
--- server_code/DatetimeHandler.py
+++ server_code/DatetimeHandler.py
@@ -1,17 +1,5 @@
 import anvil.server
-
-# This is a server module. It runs on the Anvil server,
-# rather than in the user's browser.
-#
-# To allow anvil.server.call() to call functions here, we mark
-# them with @anvil.server.callable.
-# Here is an example - you can replace it with your own:
-#
-# @anvil.server.callable
-# def say_hello(name):
-#   print("Hello, " + name + "!")
-#   return 42
-#
+from .Utils import remove_trailing_spaces
 
 class DatetimeHandler():
   def __init__(self):
