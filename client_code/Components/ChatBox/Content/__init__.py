bef7631e465c9baaa4e034f76b0f64386f4317c5
koulesali@gmail.com <koulesali@gmail.com>
2024-11-15 15:17:10 +0000

    Edited Components.ChatBox.Content

--- client_code/Components/ChatBox/Content/__init__.py
+++ client_code/Components/ChatBox/Content/__init__.py
@@ -12,16 +12,6 @@ class Content(ContentTemplate):
     # self.init_defaults()
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
-    
-    
-  def init_db(self):
-    """Getting and instance of the database"""
-    self.db = globals.DB
-    
-  def init_defaults(self):
-    """Initializes the default values of some variables"""
-    self.user = self.db.user
-    self.gb = globals
 
   def form_show(self, **event_args):
     pass
@@ -32,7 +22,3 @@ class Content(ContentTemplate):
     """Formats the label text to pretty date"""
     time = self.item['created'].strftime('%d/%m/%Y, %H:%M:%S')
     self.label_datetime.text = convert.datetime_to_pretty(datetime.strptime(time, '%d/%m/%Y, %H:%M:%S'))
-
-  def link_1_click(self, **event_args):
-    """Downloads the message's attachment"""
-    anvil.media.download(self.item['attachment'])
