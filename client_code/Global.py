ae5845eaa64c4bf4dcd14d3e85b392aeaaf65a17
koulesali@gmail.com <koulesali@gmail.com>
2024-11-29 09:46:50 +0000

    Created Global

---
+++ client_code/Global.py
@@ -0,0 +1,6 @@
+import anvil.server
+
+STORED_DATE_FORMAT = '%Y-%m-%d'
+STORED_DATETIME_FORMAT = '%Y-%m-%d, %H:%M:%S'
+DISPLAYED_DATE_FORMAT = '%b %d, %Y'
+DISPLAYED_DATETIME_FORMAT = '%b %d, %Y %H:%M'
